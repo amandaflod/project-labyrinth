2c288fc9763554e37d72343e4a44774f4fda4bb7
Liza Minicangit config --global user.email liza.minican@gmail.com <liza.minican@gmail.com>
2021-02-24 14:28:58 +0100

    added changes

--- code/src/App.js
+++ code/src/App.js
@@ -1,19 +1,33 @@
-import React from 'react'
-import { Provider } from 'react-redux';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import React from "react";
+import { Provider } from "react-redux";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { GameBegin } from "./components/GameBegin";
+import { gameDetails } from "reducers/gameDetails";
+import styled from "styled-components";
 
-import { GameBegin } from './components/GameBegin'
-import { gameDetails} from 'reducers/gameDetails'
-//import { Buttons } from './components/Buttons'
+const reducer = combineReducers({ gameDetails: gameDetails.reducer });
 
-const reducer = combineReducers({gameDetails: gameDetails.reducer });
+const store = configureStore({ reducer });
 
-const store = configureStore({reducer});
+const Wrapper = styled.section`
+  background-color: #202225;
+  color: white;
+  min-height: 400px;
+  height: 100%;
+  max-width: 400px;
+  width: 100%;
+  box-sizing: border-box;
+  font-family: "Inconsolata", monospace;
+  padding: 10px;
+  margin: 50px auto;
+`;
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <GameBegin />
+      <Wrapper>
+        <GameBegin />
+      </Wrapper>
     </Provider>
-  )
-}
+  );
+};
